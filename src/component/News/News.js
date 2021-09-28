34014c2ffe7adc8f314a477f7cf60badf580c5d0
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-28 10:21:11 +0600

    Explore News API to integrate react bootstrap Card

--- src/component/News/News.js
+++ src/component/News/News.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Col, Card } from 'react-bootstrap';
 
 const News = (props) => {
-    const { title, urlToImage, description } = props.news;
+    const { title, author, urlToImage, description } = props.news;
     return (
         <Col>
             <Card>
                 <Card.Img variant="top" src={urlToImage} />
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
+                    <Card.Title>{author}</Card.Title>
                     <Card.Text>
                         {description}
                     </Card.Text>
